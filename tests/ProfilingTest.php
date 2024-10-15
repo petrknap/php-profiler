976145381e3616a9dd79aef5af95914817d0dc37
Petr Knap <8299754+petrknap@users.noreply.github.com>
2024-10-15 20:24:05 +0200

    feat: implemented support for adding snapshot to a profile

--- tests/ProfilingTest.php
+++ tests/ProfilingTest.php
@@ -17,7 +17,26 @@ final class ProfilingTest extends TestCase
         self::assertEquals(1, round($profile->getDuration()));
     }
 
-    public function testThrowsOnSecondFinishCall(): void
+    public function testAddsSnapshotToProfile(): void
+    {
+        $profiling = Profiling::start();
+        $profiling->snapshot();
+        $profile = $profiling->finish();
+
+        self::assertCount(2 + 1, $profile->getMemoryUsages());
+    }
+
+    public function testSnapshotThrowsOnFinishedProfile(): void
+    {
+        $profiling = Profiling::start();
+        $profiling->finish();
+
+        self::expectException(Exception\ProfilingHasBeenAlreadyFinished::class);
+
+        $profiling->snapshot();
+    }
+
+    public function testFinishThrowsOnFinishedProfile(): void
     {
         $profiling = Profiling::start();
         $profiling->finish();
